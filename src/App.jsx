d98678e2fc5c68369ccd6988962d8af161581556
Aadii2504 <adityadhaygude25@gmail.com>
2024-10-20 16:36:34 +0530

    Basic Outline Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [color ,  setColor] =  useState("olive")
 
   return (
-    <>
-      
-    </>
+    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+          <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0  px-2">
+                  <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-lg">
+                 <button className="outline-none px-4 py-2 rounded-2xl font-bold shadow-xl" style={{backgroundColor: "red"}}>
+                  Red
+                 </button>
+                  </div>
+          </div>
+    </div>
   )
 }
 
